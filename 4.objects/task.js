4e63cb8f9798f00369f97814569d42067b796170
SalavatQA54 <120785005+SalavatQA54@users.noreply.github.com>
2023-06-29 15:53:58 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,39 +2,42 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
+  this.subject;
+  this.marks = new Array();
+  this.excluded;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
-Student.prototype.addMark = function (mark) {
-  if (this.marks === undefined){
-    let marks = [];
-    this.marks = [mark]
-  } else {
-    this.marks.push(mark);
-  }
+Student.prototype.addMark = function (mark)
+{
+    this.marks.push(mark);  
 }
 
-Student.prototype.addMarks = function (...rest) {
-  if (this.marks === undefined) {
-    this.marks = [...rest]
-  } else {
-    this.marks.push(...rest);
-  }
+
+Student.prototype.addMarks = function (...marks)
+{
+for(mark of marks)
+  this.marks.push(mark);
 }
 
-Student.prototype.getAverage = function () { 
-  let sum = 0;
-  for (let n = 0; n < this.marks.length; n++) {
-    sum = sum + this.marks[n];
-  }
-  return sum / this.marks.length;
+
+Student.prototype.getAverage = function ()
+{
+var sum = 0;
+this.marks.forEach(element => {
+    sum += element;
+})
+
+return (sum / this.marks.length);
 }
 
-Student.prototype.exclude = function (reason){
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+
+Student.prototype.exclude = function (reason)
+{
+delete this.subject;
+delete this.marks;
+this.excluded = reason;
 }
