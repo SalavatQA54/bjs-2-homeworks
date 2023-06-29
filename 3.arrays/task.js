5a1a23e74bc485f0e504b9b44c4fd87909ff35ae
SalavatQA54 <120785005+SalavatQA54@users.noreply.github.com>
2023-06-29 18:01:00 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,29 +1,39 @@
+//Задача №1
+
 function compareArrays(arr1, arr2) {
-    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
-}
-const people = [
-    { firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской" },
-    { firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской" },
-    { firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский" },
-    { firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский" },
-    { firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский" },
-    { firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский" },
-    { firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской" },
-    { firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской" },
-    { firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской" },
-    { firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский" },
-    { firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской" },
-    { firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской" },
-    { firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской" },
-    { firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской" },
+    if (arr1.length !== arr2.length) {
+        return false;
+      }
+    
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+    
+      return true;
+    }
 
-]
+//Задача №2
+
 function getUsersNamesInAgeRange(users, gender) {
-
-    let len = users.filter(item => item.gender == gender);
-    let result = len.map(item => item.age / len.length).reduce((a, b) => a + b);
-
-    return result;
-
+    const filteredUsers = users.filter(function(user) {
+        return user.gender === gender;
+    });
+    
+    const ages = filteredUsers.map(function(user) {
+        return user.age;
+    });
+    
+    const sum = ages.reduce(function(total, age) {
+        return total + age;
+    }, 0);
+    
+    const averageValue = sum / ages.length;
+    
+    if (isNaN(averageValue)) {
+        return 0;
+    } else {
+        return averageValue;
+    }
 }
-getUsersNamesInAgeRange(people, "женский");
