80ba2a71e193a954a2ac5b8a8939d5d2d6401e0d
SalavatQA54 <120785005+SalavatQA54@users.noreply.github.com>
2023-06-29 15:51:51 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined){
+    let marks = [];
+    this.marks = [mark]
+  } else {
+    this.marks.push(mark);
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    this.marks = [...rest]
+  } else {
+    this.marks.push(...rest);
+  }
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.getAverage = function () { 
+  let sum = 0;
+  for (let n = 0; n < this.marks.length; n++) {
+    sum = sum + this.marks[n];
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason){
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
