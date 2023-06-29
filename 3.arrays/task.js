321528c0a9944331d8fdda0831f477d89ac34d02
SalavatQA54 <120785005+SalavatQA54@users.noreply.github.com>
2023-06-29 16:10:17 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,12 +1,13 @@
 function compareArrays(arr1, arr2) {
-  if (arr1.length === arr2.length)
-    return arr1.every((element, index) => element === arr2[index]);
-  else return false;
+
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+
+  return result; // boolean
 }
-// let result = JSON.stringify(arr1)==JSON.stringify(arr2); //ещё один рабочий вариант
-// return result;
 
 function advancedFilter(arr) {
-  let result = arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i * 10)
-  return result;
+  
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 == 0).map(number => number * 10);
+
+  return resultArr; // array
 }
