2636fd08379b1225ceff42f638be3d03302988d5
SalavatQA54 <120785005+SalavatQA54@users.noreply.github.com>
2023-06-29 17:57:59 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,29 @@
 function compareArrays(arr1, arr2) {
-  let isEqual =
-    arr1.length === arr2.length &&
-    arr1.every((value, index) => value === arr2[index])
-  return isEqual
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
+const people = [
+    { firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской" },
+    { firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской" },
+    { firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский" },
+    { firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский" },
+    { firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский" },
+    { firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский" },
+    { firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской" },
+    { firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской" },
+    { firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской" },
+    { firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский" },
+    { firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской" },
+    { firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской" },
+    { firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской" },
+    { firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской" },
 
+]
 function getUsersNamesInAgeRange(users, gender) {
-  let result = users
-    .filter((user) => user.gender === gender)
-    .map((user) => user.age)
-    .reduce((previous, current, currentIndex, array) => {
-      previous += current
-      if (currentIndex === array.length - 1) {
-        return previous / array.length
-      }
-      return previous
-    }, 0)
-  return result
+
+    let len = users.filter(item => item.gender == gender);
+    let result = len.map(item => item.age / len.length).reduce((a, b) => a + b);
+
+    return result;
+
 }
+getUsersNamesInAgeRange(people, "женский");
