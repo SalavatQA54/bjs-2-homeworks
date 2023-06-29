725a6152d31dbecaa017a98e081a95e6ded162ea
SalavatQA54 <120785005+SalavatQA54@users.noreply.github.com>
2023-06-29 18:03:42 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,43 +1,31 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.subject;
-  this.marks = new Array();
-  this.excluded;
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
-}
-
-Student.prototype.addMark = function (mark)
-{
-    this.marks.push(mark);  
-}
-
-
-Student.prototype.addMarks = function (...marks)
-{
-for(mark of marks)
-  this.marks.push(mark);
-}
-
-
-Student.prototype.getAverage = function ()
-{
-var sum = 0;
-this.marks.forEach(element => {
-    sum += element;
-})
-
-return (sum / this.marks.length);
-}
-
-
-Student.prototype.exclude = function (reason)
-{
-delete this.subject;
-delete this.marks;
-this.excluded = reason;
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
+  
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  };
+  
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.excluded) {
+      this.marks.push(...marksToAdd);
+    }
+  };
+  
+  Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+  
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
+  };
+  
+  Student.prototype.exclude = function(reason) {
+    this.subject = undefined;
+    this.marks = undefined;
+    this.excluded = reason;
+  };
